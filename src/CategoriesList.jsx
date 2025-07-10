f7c9ec846fb7e41a5249dbe48fdc33b1e95b101a
Mathew Ritchie <ritchmat89@gmail.com>
2025-07-10 10:15:18 +0200

    added jsx for CategoriesList.

--- src/CategoriesList.jsx
+++ src/CategoriesList.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
 import useRecipeStore from "./useRecipeStore";
 
 export default function CategoriesList() {
@@ -11,7 +12,15 @@ export default function CategoriesList() {
 
   return (
     <div>
-      <h1>Hello</h1>
+      {categories.length > 0 && (
+        <div className="flex flex-wrap justify-center items-center gap-5 mx-10">
+          {categories.map((category) => (
+            <Link key={category.idCategory}>
+              <p>{category.strCategory}</p>
+            </Link>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
