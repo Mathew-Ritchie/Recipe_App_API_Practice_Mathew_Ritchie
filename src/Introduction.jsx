4792cb74cd3a4b23a102d8799441c588a9453620
Mathew Ritchie <ritchmat89@gmail.com>
2025-07-20 18:39:51 +0200

    Word amendment to introduction.

--- src/Introduction.jsx
+++ src/Introduction.jsx
@@ -4,7 +4,7 @@ export default function Introduction() {
   return (
     <div className="w-screen text-center px-10 sm:px-10 md:px-20 lg:px-52">
       <h2 className="text-xl lemon-regular text-gray-700 ">
-        Welcome to My Recipe App, a virtual recipe book created with The MealDB API. Please explore
+        Welcome to My Recipe Book, a virtual recipe book created with The MealDB API. Please explore
         the recipes with the help of the letter selector or categories selector, then click on a
         recipe when you find something you like!
       </h2>
